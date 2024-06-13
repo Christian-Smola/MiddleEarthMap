42ac0405136cca3380606501f8caf8b5768018ea
Christian-Smola <c.smola@yahoo.com>
2024-06-12 23:36:29 -0500

    Added eastern Gondor

--- Middle Earth Map/Assets/Resources/Scripts/Map.cs
+++ Middle Earth Map/Assets/Resources/Scripts/Map.cs
@@ -163,6 +163,20 @@ public class Map : MonoBehaviour
                     area.ProvinceList.Add(new Province(new Color32(102, 255, 0, 255), Nation.Find("Rohan")));
 
                     //Gondor
+                    area = Area.Find("Lossarnach");
+
+                    area.ProvinceList.Add(new Province(new Color32(255, 0, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 255, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 0, 255, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(255, 255, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(255, 0, 255, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 255, 255, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(102, 0, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 102, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 0, 102, 255), Nation.Find("Gondor")));
+
                     area = Area.Find("Anorien");
 
                     area.ProvinceList.Add(new Province(new Color32(255, 0, 0, 255), Nation.Find("Gondor")));
@@ -179,6 +193,32 @@ public class Map : MonoBehaviour
 
                     area.ProvinceList.Add(new Province(new Color32(102, 102, 0, 255), Nation.Find("Gondor")));
                     area.ProvinceList.Add(new Province(new Color32(102, 0, 102, 255), Nation.Find("Gondor")));
+
+                    area = Area.Find("Lebennin");
+
+                    area.ProvinceList.Add(new Province(new Color32(255, 0, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 255, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 0, 255, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(255, 255, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(255, 0, 255, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 255, 255, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(102, 0, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 102, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 0, 102, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(102, 102, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(102, 0, 102, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 102, 102, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(255, 102, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(255, 0, 102, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(102, 255, 0, 255), Nation.Find("Gondor")));
+                    area.ProvinceList.Add(new Province(new Color32(0, 255, 102, 255), Nation.Find("Gondor")));
+
+                    area.ProvinceList.Add(new Province(new Color32(102, 0, 255, 255), Nation.Find("Gondor")));
                 }
 
                 public Province(Color32 col, Nation owner) => (color, Owner) = (col, owner);
@@ -201,7 +241,9 @@ public class Map : MonoBehaviour
 
                 region = Region.Find("Gondor");
 
-                region.AreaList.Add(new Area("Anorien", new Color32(255, 0, 0, 255)));
+                region.AreaList.Add(new Area("Lossarnach", new Color32(255, 0, 0, 255)));
+                region.AreaList.Add(new Area("Anorien", new Color32(0, 255, 0, 255)));
+                region.AreaList.Add(new Area("Lebennin", new Color32(0, 0, 255, 255)));
 
                 Province.PopulateProvinceLists();
             }
